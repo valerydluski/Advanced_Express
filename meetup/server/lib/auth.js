f39dd0534d713950aebdaa2de6d3d53ab8e29848
Valery Dluski <valerydluski@gmail.com>
2021-02-20 15:53:42 +0300

    feat: Setting up an authentication strategy for Passport

--- meetup/server/lib/auth.js
+++ meetup/server/lib/auth.js
@@ -1,4 +1,24 @@
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/UserModel');
+
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, async (username, password, done) => {
+    try {
+      const user = await UserModel.findOne({ email: username }).exec();
+      if (!user) {
+        return done(null, false, { message: 'Invalid username or password' });
+      }
+      const passwordOK = await user.comparePassword(password);
+      if (!passwordOK) {
+        return done(null, false, { message: 'Invalid username or password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
 
 module.exports = {
   initialize: passport.initialize(),
